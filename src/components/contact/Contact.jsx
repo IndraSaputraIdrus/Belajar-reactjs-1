75453ea3a2e72a27654f29596a595277a7e5fd6b
Indra-Saputra-Idrus <indrasaputraidrus@gmail.com>
2022-03-09 22:20:50 +0800

    Contact section

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,10 +1,33 @@
 import "./contact.css"
 import Section from "../section/Section"
+import {useState} from "react";
 
 const Contact = () => {
+	const [message, setMessage] = useState(false);
+
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		setMessage(true);
+
+		alert("Thanks, I,ll reply ASAP");
+	}
+
 	return (
 		<Section id="contact" className="contact">
-			contact
+		<div className="container">
+			
+			<div className="left">
+				<img className="img-contact" src="/assets/shake.svg" alt="" />
+			</div>
+			<div className="right">
+				<h2>Contact</h2>
+				<form action="" className="contact-form" onSubmit={handleSubmit}>
+					<input type="text" name="email" id="email" placeholder="Email" autoComplete="off"/>
+					<textarea name="message" id="message"></textarea>
+					<button type="submit">Send</button>
+				</form>
+			</div>
+		</div>
 		</Section>
 	)
 }
